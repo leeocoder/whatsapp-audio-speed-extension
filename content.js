9e8d4257c12b5f0dc49b104a044dfd79b093643b
Leonardo Albuquerque <leeodesign@hotmail.com>
2020-08-15 22:25:29 -0300

    *All done

--- content.js
+++ content.js
@@ -1,1 +1,24 @@
-console.log("rodando....");
+//Verify if the header is loaded
+const interval = setInterval(() => {
+  //Select the button to change speed
+  const headerPerfil = document.querySelector("div._1MXsz");
+  if (headerPerfil) {
+    console.log(headerPerfil);
+    clearInterval(interval);
+  }
+  //Create a button
+  const button = document.createElement("button");
+  button.classList.add("btnChanger");
+  button.innerHTML = "2x";
+
+  //Add event's button
+  button.addEventListener("click", () => {
+    const audios = document.querySelectorAll("audio");
+    audios.forEach((audio) => {
+      audio.playbackRate = 2;
+    });
+  });
+
+  //Add Button to change speed
+  headerPerfil.appendChild(button);
+}, 1000);
